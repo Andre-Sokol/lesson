9f5db7f12deeb341aa661611ef2adde1297d6682
Andre-Sokol <tehburoandre@mail.ru>
2024-08-14 08:48:13 +0500

    Delete module_2_2.py

--- module_2_2.py
+++
@@ -1,9 +0,0 @@
-first = input('первое число: ')
-second = input('второе число: ')
-third = input('третье число: ')
-if first==second and first==third and second==third:
-    print(3)
-elif first==second or first==third or second==third:
-    print(2)
-else:
-    print(0)
