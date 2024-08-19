53a76e3b5efc7ef7d3ad30018b5c704a4400821d
Andre-Sokol <tehburoandre@mail.ru>
2024-08-19 14:38:14 +0500

    Add files via upload

---
+++ module_3_1.py
@@ -0,0 +1,25 @@
+calls = 0
+
+
+def count_calls():
+    global calls
+    calls += 1
+
+
+def string_info(string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+
+
+def is_contains(string, list_to_search):
+    count_calls()
+    return string.upper() in [i.upper() for i in list_to_search]
+
+
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycle', 'cyclic']))
+print(calls)
