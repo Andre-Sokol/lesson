76713f9bbcce6785e6eee1876dfbf13426398e20
Andre-Sokol <tehburoandre@mail.ru>
2024-08-21 08:09:53 +0500

    Delete module_3_3.py

--- module_3_3.py
+++
@@ -1,7 +0,0 @@
-def print_params(a=1, b='строка', c=True):
-    print(a, b, c)
-
-
-values_list = [2, 'Dima', False]
-values_dict = {'a': 5, 'b': 'Alex', 'c': False}
-print_params(*values_list, **values_dict)
