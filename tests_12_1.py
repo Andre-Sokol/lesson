2614f4668d399e809a447eff9bdec13d3512d58b
Andre-Sokol <tehburoandre@mail.ru>
2024-11-28 11:36:16 +0500

    Простые Юнит-Тесты
    Задача "Проверка на выносливость":
    В первую очередь скачайте исходный код, который нужно обложить тестами с GitHub. (Можно скопировать)
    В этом коде сможете обнаружить класс Runner, объекты которого вам будет необходимо протестировать.
    
    Напишите класс RunnerTest, наследуемый от TestCase из модуля unittest. В классе пропишите следующие методы:
    test_walk - метод, в котором создаётся объект класса Runner с произвольным именем. Далее вызовите метод walk у этого объекта 10 раз. После чего методом assertEqual сравните distance этого объекта со значением 50.
    test_run - метод, в котором создаётся объект класса Runner с произвольным именем. Далее вызовите метод run у этого объекта 10 раз. После чего методом assertEqual сравните distance этого объекта со значением 100.
    test_challenge - метод в котором создаются 2 объекта класса Runner с произвольными именами. Далее 10 раз у объектов вызываются методы run и walk соответственно. Т.к. дистанции должны быть разными, используйте метод assertNotEqual, чтобы убедится в неравенстве результатов.
    Запустите кейс RunnerTest. В конечном итоге все 3 теста должны пройти проверку.
    
    Пункты задачи:
    Скачайте исходный код для тестов.
    Создайте класс RunnerTest и соответствующие описанию методы.
    Запустите RunnerTest и убедитесь в правильности результатов.
    Пример результата выполнения программы:
    Вывод на консоль:
    Ran 3 tests in 0.001s OK
    
    Примечания:
    Попробуйте поменять значения в одном из тестов, результаты
    Файл tests_12_1.py с классом тестов загрузите на ваш GitHub репозиторий. В решении пришлите ссылку на него.
    Успехов!

--- tests_12_1.py
+++ tests_12_1.py
@@ -18,12 +18,16 @@ class Runner:
     #     self.distance += 50
 
 class RunnerTest(unittest.TestCase):
+    is_frozen = False
+
+    @unittest.skipUnless(is_frozen, 'Тесты в этом кейсе заморожены.')
     def test_walk(self):
         runner = Runner("John")
         for i in range(10):
             runner.walk()
         self.assertEqual(runner.distance, 50)
 
+    @unittest.skipUnless(is_frozen, 'Тесты в этом кейсе заморожены.')
     def test_run(self):
         runner = Runner("Denis")
         for i in range(10):
