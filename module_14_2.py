2ea76578a113871edc3999c52a6571482d7153ec
Andre-Sokol <tehburoandre@mail.ru>
2024-12-15 19:09:06 +0500

    Выбор элементов и функции в SQL запросах
    Задача "Средний баланс пользователя":
    Для решения этой задачи вам понадобится решение предыдущей.
    Для решения необходимо дополнить существующий код:
    Удалите из базы данных not_telegram.db запись с id = 6.
    Подсчитать общее количество записей.
    Посчитать сумму всех балансов.
    Вывести в консоль средний баланс всех пользователей.

---
+++ module_14_2.py
@@ -0,0 +1,55 @@
+import sqlite3
+
+connection = sqlite3.connect("not_telegram.db")
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS users (
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER
+)
+''')
+
+cursor.execute("CREATE INDEX IF NOT EXISTS idx_email ON users (email)")
+
+# Заполните её 10 записями:
+for i in range(1, 11):
+    cursor.execute("INSERT INTO Users(username, email, age, balance) values (?, ?, ?, ?)",
+                   (f"User{i}", f"example{i}@gmail.com", i * 10, 1000)) # добавление таблицы
+
+# Обновите balance у каждой 2ой записи начиная с 1ой на 500:
+users = cursor.execute("SELECT username, id FROM Users WHERE id % 2  != 0")
+for user in users.fetchall():
+    cursor.execute('UPDATE Users SET balance = ? WHERE username = ?', (500, user[0]))
+
+# Удалите каждую 3ую запись в таблице начиная с 1ой:
+cursor.execute("DELETE FROM Users WHERE id % 3 == 1")
+
+# Выбрать все записи, где возраст не равен 60
+cursor.execute("SELECT * FROM Users WHERE age!=60")
+results = cursor.fetchall()
+
+# Вывести результаты в консоль
+for result in results:
+    print(f"Имя: {result[1]} | Почта: {result[2]} | Возраст: {result[3]} | Баланс: {result[4]}")
+
+# удалить запись с id = 6
+cursor.execute("DELETE FROM Users WHERE id == 6")
+
+# Подсчитать общее количество записей
+cursor.execute("SELECT count(*) FROM Users")
+quantity_balance = cursor.fetchone()[0]
+
+# Посчитать сумму всех балансов.
+cursor.execute("SELECT sum(balance) FROM Users")
+sum_balance = cursor.fetchone()[0]
+
+# средний баланс всех пользователей
+print(sum_balance / quantity_balance)
+
+
+connection.commit()
+connection.close()
