1ba4c4a1b727f60d6d401261567bff33774bf253
Andre-Sokol <tehburoandre@mail.ru>
2025-01-14 09:39:25 +0500

    Update module_16_1.py

--- module_16_1.py
+++ module_16_1.py
@@ -9,19 +9,20 @@ app = FastAPI()
 async def Get_Main_Page():
     return {"message": "Главная страница"}
 
+# маршрут к странице администратора
 @app.get("/user/admin")
 async def Get_admin_Ppage():
     return {"message": "Вы вошли как администратор."}
 
+# маршрут к страницам пользователей
 @app.get("/user/{user_id}")
 async def Get_User_Number(user_id):
     return {"message": f"Вы вошли как пользователь № {user_id}"}
 
+# маршрут к страницам пользователей передавая данные в адресной строке
 @app.get("/user")
 async def Get_User_Info(username: str, age: int):
     return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
 
-# @app.get("/user/frist_name/last_name")
-# async def new(frist_name: str, last_name: str) -> dict:
-#     return {"message": f"Привет, {frist_name} {last_name}"}
+
 
