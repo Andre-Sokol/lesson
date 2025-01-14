66dd15533d5f113ea0c9d6e1fe969d567b338468
Andre-Sokol <tehburoandre@mail.ru>
2025-01-14 11:58:50 +0500

    Валидация данных
    Задача "Аннотация и валидация":
    Допишите валидацию для маршрутов из предыдущей задачи при помощи классов Path и Annotated:
    '/user/{user_id}' - функция, выполняемая по этому маршруту, принимает аргумент user_id, для которого необходимо написать следующую валидацию:
    Должно быть целым числом
    Ограничено по значению: больше или равно 1 и меньше либо равно 100.
    Описание - 'Enter User ID'
    Пример - '1' (можете подставить свой пример не противоречащий валидации)
    '/user' замените на '/user/{username}/{age}' - функция, выполняемая по этому маршруту, принимает аргументы username и age, для которых необходимо написать следующую валидацию:
    username - строка, age - целое число.
    username ограничение по длине: больше или равно 5 и меньше либо равно 20.
    age ограничение по значению: больше или равно 18 и меньше либо равно 120.
    Описания для username и age - 'Enter username' и 'Enter age' соответственно.
    Примеры для username и age - 'UrbanUser' и '24' соответственно. (можете подставить свои примеры не противоречащие валидации).

---
+++ module_16_2.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Path
+
+# Создаем экземпляр приложения FastAPI
+app = FastAPI()
+
+
+@app.get("/user/{user_id}")
+async def Get_User_Number(user_id: int = Path(ge=1, le=100, description="Enter User ID", example='1')):
+    return {"message": f"Вы вошли как пользователь № {user_id}"}
+
+
+# маршрут к страницам пользователей
+@app.get("/user/{username}/{age}")
+async def Get_User_Name_age(
+        username: str = Path(min_length=5, max_length=20, description="Enter username", example='UrbanUser'),
+        age: int = Path(ge=18, le=120, description="Enter age", example='24')):
+    return {"message": f"Hellow {username}, возраст {age} "}
